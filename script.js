6baa7051a0c37db3f779014f27c3d635ecc375a3
deathjudge1 <mobayoomotara3@gmail.com>
2024-02-28 13:51:23 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const countDowndate = new Date("march 28, 2024 00:00:00").getTime();
+const x =setInterval(function(){
+    const now = new Date().getTime();
+    const distance = countDowndate - now
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+
+
+},1000);
